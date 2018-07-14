c562a63ddef655067a2ba0945854acf21bbb50e9
Jose Alejandro Montana <jamontanac@unal.edu.co>
2018-07-14 15:25:53 -0500

    Correction on the program

--- Prueba.py
+++ Prueba.py
@@ -51,15 +51,16 @@ def bra(a,b):
     return np.matrix([[a.conjugate(),b.conjugate()]])
 #----------------------method-------------------------------------------
 def euler(a,b,t,dt,dW):
-    A1=((np.identity(2)-Jdag()*J()
-             # /2.0-(1.0*1j/hbar)*H(t))*ket(a,b))[0,0]
+    A1=((np.identity(2)-Jdag()*J()))
+    return A1
+    # /2.0-(1.0*1j/hbar)*H(t))*ket(a,b))[0,0]
     # A2=((np.identity(2)-Jdag()*J()/2.0-(1.0*1j/hbar)*H(t))*ket(a,b))[1,0]
     # B1=(J()*ket(a,b))[0,0]
     # B2=(J()*ket(a,b))[1,0]
     # a=a+A1*dt+B1*dW
     # b=b+A2*dt+B2*dW
     # return a,b
-    return A1
+   
 # std::complex<double> Numero (1.0,2.0);
 # std::complex<double> Dos (1.0,3.0);
 
