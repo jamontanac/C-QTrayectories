2c61933131dbba1048e73b81ba84c1ff6a5c4ba7
Jose Alejandro Montana <jamontanac@unal.edu.co>
2018-07-15 00:55:54 -0500

    Here there is the implementantion of the program that computes the termodynamic quantities and the block coordinates

--- Secondpart.cpp
+++ Secondpart.cpp
@@ -3,7 +3,9 @@
 #include <complex>
 #include "armadillo"
 #include "omp.h"
+
 // --------------------- defining constants --------------------
+
 const double Hbar = 1.0;
 const double Epsilon=1.0;
 const double Nu = 8.0;
@@ -15,7 +17,9 @@ const double Tmax=Tau;
 const int N=Tmax/dt;
 const double Sigma=sqrt(dt);
 const double Mu=0.0;
+
 // ----------- Defining as constants the pauli matrices ----------------
+
 const arma::cx_mat Sigma_x={{0.0,std::complex<double> (1.0,0.0)},{std::complex<double> (1.0,0.0),0.0}};
 const arma::cx_mat Sigma_y={{0.0,std::complex<double> (0.0,1.0)},{std::complex<double> (0.0,-1.0),0.0}};
 const arma::cx_mat Sigma_z={{std::complex<double> (-1.0,0.0), 0.0},{ 0.0,std::complex<double> (1.0,0.0)}};
@@ -23,7 +27,9 @@ const arma::cx_mat Sigma_plus={{0.0, 0.0},{ std::complex<double> (1.0,0.0),0.0}}
 const arma::cx_mat Sigma_minous={{ 0.0,std::complex<double> (1.0,0.0)},{ 0.0,0.0}};
 const arma::cx_mat J= sqrt(Gamma)*Sigma_z;
 const arma::cx_mat Jdagga= J.t();
+
 // ----------------------- Defining Functions ----------------------
+
 double Lambda(double time);
 arma::cx_mat Hamiltonian(double time);
 arma::cx_mat Ket(std::complex<double> x1 , std::complex<double> x2);
@@ -31,6 +37,9 @@ arma::cx_mat Bra(std::complex<double> x1 , std::complex<double> x2);
 arma::cx_mat Euler(std::complex<double> & x1,std::complex<double> &x2,double time,double dTime,double dW);
 arma::cx_mat State(std::complex<double> x1,std::complex<double> x2);
 arma::cx_mat Normalise(arma::cx_mat Not_normalised);
+
+// ------------------------------------------------------------
+
 int main(int argc, char **argv)
 {
   std::cout.precision(16);
@@ -46,40 +55,42 @@ int main(int argc, char **argv)
 
   // -------------------------------------------------
 
-  // -----------------thing to use----------------------
+  // -----------------things to use----------------------
   arma::cx_mat rho1 (Size,Size);
   arma::cx_mat rho2 (Size,Size);
   arma::cx_mat ketstate (Size,1);
+  arma::cx_mat dHamiltonian (Size,Size);
+  arma::cx_mat drho (Size,Size);
   double dXi = 0.0;
+  double Work = 0.0;
+  double Energy = 0.0;
+  double X_block=0.0;
+  double Y_block=0.0;
+  double Z_block=0.0;
   
   // int Trajectories=atoi(argv[1]);
 
-  // ---------------------------------------------------------
-  // std::mt19937 generator(std::random_device{}()); //std::mersenne_twister_engine set with seed the time of the computer with _64 we set to 64 bits
-  // std::uniform_int_distribution<int> distribution(-100,100);//make a uniform distribution
-  // ---------------------------------------------------------------
 
-   rho1= State(x0,y0);
-   dXi=Noise(generator);
-   ketstate=Euler(x0,y0,t,dt,dXi);
-   std::cout<<ketstate<<std::endl;
-   std::cout<<ketstate.t()*ketstate<<std::endl;
-   ketstate=Normalise(ketstate);
-   std::cout<<ketstate<<std::endl;
-   std::cout<<ketstate.t()*ketstate<<std::endl;
-   // std::cout<<rho1<<std::endl;
-
-
+  for(int i=0; i< N; i++)
+    {
+      rho1 = State(x0,y0);
+      dXi = Noise(generator);
+      ketstate = Euler(x0,y0,t,dt,dXi);
+      ketstate = Normalise(ketstate);
+      rho2 = State(ketstate(0,0),ketstate(1,0));
+      dHamiltonian = Hamiltonian(t+dt)-Hamiltonian(t);
+      drho = rho2 - rho1;
+      Work = real(arma::trace(rho2*dHamiltonian));
+      Energy = real(arma::trace(Hamiltonian(t+dt)*rho2)-arma::trace(Hamiltonian(t)*rho1));
+      X_block = real(arma::trace(rho1*Sigma_x));
+      Y_block = real(arma::trace(rho1*Sigma_y));
+      Z_block = real(arma::trace(rho1*Sigma_z));
+      std::cout<<t<<"  "<< X_block<<"  "<< Y_block<<"  "<< Z_block<<std::endl;
+      t += dt;
+      x0 = ketstate(0,0);
+      y0 = ketstate(1,0);
+    }
   
-  // std::complex<double> Numero (1.0,2.0);
-  // std::complex<double> Dos (1.0,3.0);
-  
-  // arma:: cx_mat A=Euler(Numero,Dos,10,0.1,0.02);
-  // arma:: cx_mat A=State(Numero,Dos);
-  // std::cout<<arma::as_scalar(Bra(Numero,Dos)*Ket(Numero,Dos))<<std::endl;
-  // std::cout<<A(0,0)<<"  "<< A(1,0)<<std::endl;
-  // std::cout<<Normalise(A)<<std::endl;
-  // std::cout<<A<<std::endl;
   return 0;
 }
 // --------------- Implementing functions ----------------
@@ -115,7 +126,10 @@ arma::cx_mat Euler(std::complex<double> & x1,std::complex<double> & x2,double ti
   arma::cx_mat A = (Id - Jdagga*J*0.5 - (Iimag/Hbar)*Hamiltonian(time))*Ket(x1,x2);
   arma::cx_mat B = J*Ket(x1,x2);
   arma::cx_mat eulermethod = A*dTime+B*dW;
-  return eulermethod;
+  x1 += eulermethod(0,0);
+  x2 += eulermethod(1,0);
+  // return eulermethod;
+    return Ket(x1,x2);
 }
 arma::cx_mat State(std::complex<double> x1,std::complex<double> x2)
 {
