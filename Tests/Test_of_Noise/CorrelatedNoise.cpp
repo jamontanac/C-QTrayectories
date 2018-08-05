36ca65567a08abd07a7e8e8fa6c8a7bee8cf0ebf
Jose Alejandro Montana <jamontanac@unal.edu.co>
2018-08-05 01:43:44 -0500

    Modifications in order to work for the unravelings u

--- Tests/Test_of_Noise/CorrelatedNoise.cpp
+++ Tests/Test_of_Noise/CorrelatedNoise.cpp
@@ -6,6 +6,7 @@
 // #include "omp.h"
 const double Sigma=0.1;
 const double Mu=0.0;
+const double dt = 0.01;
 const arma::cx_mat Sigma_y={{0.0,std::complex<double> (0.0,1.0)},{std::complex<double> (0.0,-1.0),0.0}};
 const double Gamma = 5e-3;
 std::complex<double> Correlated_noise (double Noise1, double Noise2);
@@ -29,8 +30,10 @@ std::complex<double> Correlated_noise (double Noise1, double Noise2)
 {
   arma::vec Eigenvalues;
   arma::mat Eigenvectors;
-  arma::mat Matrix_Variance ={{1.0,0.7},{0.7,1.0}};
+  std::complex<double> U_corr (1.0,0.0);
   arma::vec target_mean  = {0.0, 0.0};
+  arma::mat Matrix_Variance = {{1.0+real(U_corr),imag(U_corr)},{imag(U_corr),1.0-real(U_corr)}};
+  Matrix_Variance=(dt/2)*Matrix_Variance;
   eig_sym(Eigenvalues, Eigenvectors, Matrix_Variance);
   arma::mat l = sqrt(diagmat(Eigenvalues));
   arma::mat Q = Eigenvectors * l;
@@ -38,8 +41,10 @@ std::complex<double> Correlated_noise (double Noise1, double Noise2)
   original.zeros();
   arma::vec tweaked (2);
   original={Noise1,Noise2};
-  // std::cout << Q<<std::endl;
+  // // std::cout << Q<<std::endl;
   tweaked= (Q*original + target_mean);
   std::complex<double> Cor_Noise (tweaked(0),tweaked(1));
   return Cor_Noise;
+  // std::complex<double> x (0.0,0.0)
+  // return U_corr;
 }
